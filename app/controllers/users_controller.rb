d8b3a3441441e928c4b707ab90bc046d8be9c00b
cema-sp <cema.rus@gmail.com>
2014-10-08 23:24:13 +0400

    adding minor changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+ 		# deal with it
+ 		render 'new'
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+  	# Returns filtered and safe params
+  	def user_params
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	end
 end
