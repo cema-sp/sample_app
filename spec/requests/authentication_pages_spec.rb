4e03de65aea0776a73ac0675c5d00e0f549aaf26
cema-sp <cema.rus@gmail.com>
2014-10-15 17:24:48 +0400

    moving forward..

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -29,6 +29,7 @@ RSpec.describe "AuthenticationPages", :type => :request do
 
       it { should have_content("Sign In") }
       it { should have_error_message "Invalid" }
+      it { should_not have_link('Settings') }
     
       describe "after visiting another page" do
         before { click_link "Home" }
@@ -46,11 +47,13 @@ RSpec.describe "AuthenticationPages", :type => :request do
       it { should have_title(user.name) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
       it { should_not have_link('Sign in', href: signin_path) }
 
       describe "after signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
+        it { should_not have_link('Sign out') }
       end
     end
   end
